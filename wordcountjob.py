c3961d5e97fa9223770e84135076bd9ae3cf10b9
rishabh verma <vermarishabh0987@gmail.com>
2018-10-21 00:31:20 +0530

    doing sorting and filtering only word
    we have "rishabh , " and  "rishabh"
    this is one word but counted seprately
    so to deal with this ...
    
    and to sort the result

--- wordcountjob.py
+++ wordcountjob.py
@@ -1,15 +1,27 @@
+import re
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
+#only allowing proper words eg:"rishabh ," as 1 and "rishabh" as 1 to only "rishabh" as 1
+def normalize(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
 input = sc.textFile("file:///Bigdata/WordCountJob/book.txt")
-words = input.flatMap(lambda x: x.split())
+words = input.flatMap(normalize)
 
-wordCounts = words.countByValue()
+countwords = words.map(lambda x: (x,1)).reduceByKey(lambda x,y : x+y)
+#flip key and value then sort by key
+countwordsSorted = countwords.map(lambda x: (x[1],x[0])).sortByKey()
 
-for word, count in wordCounts.items():
-   cleanWord = word.encode('ascii', 'ignore')
-   if (cleanWord):
-       print(cleanWord.decode() + " " + str(count))
- 
+
+results = countwordsSorted.collect()
+
+for result in results:
+    count = str(result[0])
+    word = result[1].encode('ascii','ignore')
+    if (word):
+        print(word.decode() + "\t" + count)
+    
+    
+
