152cdafd0a3a615d4598395b1d0f013e46e0d21b
rishabh verma <vermarishabh0987@gmail.com>
2018-10-20 23:32:50 +0530

    Update wordcountjob.py

--- wordcountjob.py
+++ wordcountjob.py
@@ -5,9 +5,11 @@ sc = SparkContext(conf = conf)
 
 input = sc.textFile("file:///sparkcourse/book.txt")
 words = input.flatMap(lambda x: x.split())
+
 wordCounts = words.countByValue()
 
 for word, count in wordCounts.items():
-    cleanWord = word.encode('ascii', 'ignore')
-    if (cleanWord):
-        print(cleanWord.decode() + " " + str(count))
+   cleanWord = word.encode('ascii', 'ignore')
+   if (cleanWord):
+       print(cleanWord.decode() + " " + str(count))
+ 
